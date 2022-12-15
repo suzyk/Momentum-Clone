29107a1a6a2b427e9e72dec77f07bf7c1cd4992c
suzyk <suzykahng@gmail.com>
2022-12-15 22:52:11 +0300

    custom checkbox added

--- JS/todo.js
+++ JS/todo.js
@@ -3,6 +3,7 @@ const todoList = document.querySelector("#todo-list");
 const todoInput = todoForm.querySelector("input");  // OR (#todo-form input)
 let toDos = [];
 const TODOS_KEYS = "todos";
+const LINE_THROUGH_CLASSNAME = "line-through";
 
 function saveTodos(){
     localStorage.setItem(TODOS_KEYS, JSON.stringify(toDos));
@@ -12,7 +13,7 @@ function handleSubmit(event){
     const newTodo = todoInput.value;
     
     todoInput.value = "";
-    const todoObj = {item : newTodo, id: Date.now()};
+    const todoObj = {item : newTodo, id: Date.now(), completed: false};
     toDos.push(todoObj);
     
     paintTodo(todoObj);
@@ -26,18 +27,44 @@ function handleDeleteItem(event){
     saveTodos();    // we need to update local storage every time we update the array.
 }
 
+function handelCheckboxClick(event){
+    const todoLabel = event.target.nextSibling;
+    if (this.checked) {
+        todoLabel.classList.add(LINE_THROUGH_CLASSNAME);
+    }else{
+        todoLabel.classList.remove(LINE_THROUGH_CLASSNAME);
+    }
+    const li = event.currentTarget.parentNode; 
+    const todoIndex = toDos.findIndex((todo) => todo.id == li.id);
+    console.log(`index ${todoIndex}`);
+    toDos[todoIndex].completed = !toDos[todoIndex].completed;
+
+    saveTodos();
+}
+
 function paintTodo(todoObj){
     const li = document.createElement("li");
     const span = document.createElement("span");
     const deleteBtn = document.createElement("button");
     const btnIcon = document.createElement("i");
+    const checkBox = document.createElement("input");
+    checkBox.type = "checkbox";
+    checkBox.id = "checkBox";
     btnIcon.classList.add("fa-regular", "fa-trash-can");
     deleteBtn.appendChild(btnIcon);
     deleteBtn.addEventListener("click", handleDeleteItem);
+    if (todoObj.completed) {
+        span.classList.add(LINE_THROUGH_CLASSNAME);
+        checkBox.checked = true;
+    }
     li.id = todoObj.id;
+    li.classList.add("todoItem");
+    li.appendChild(checkBox);
     li.appendChild(span);
     li.appendChild(deleteBtn);
     todoList.appendChild(li);
+
+    checkBox.addEventListener("change", handelCheckboxClick);
 
     span.innerText = todoObj.item;
 }
